a61a96ade0fed65346f970659bbe395bf6fafc11
alexthered <hiep.dv179@gmail.com>
2014-11-25 00:02:23 +0100

    Add class for detecting region containg plate from input image

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include "platedetector.h"
 #include <iostream>
+
+using namespace std;
+using namespace cv;
+
 
 int main()
 {
-	std::cout << "Haha";
-	return 1;
+    /* read input image */
+    Mat in_img = imread("../../data/P6070015.jpg");
+
+    namedWindow("Input image", CV_WINDOW_NORMAL || CV_WINDOW_KEEPRATIO || CV_GUI_EXPANDED);
+    cv::imshow("Input image", in_img);
+    waitKey(0);
 }
---
+++ platedetector.h
@@ -0,0 +1,19 @@
+#ifndef PLATEDETECTOR_H
+#define PLATEDETECTOR_H
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+/**
+ * Extract region containg plate from an input image
+ *
+ **/
+
+class PlateDetector
+{
+public:
+    PlateDetector();
+
+};
+
+#endif // IMGPREPROCESSOR_H
