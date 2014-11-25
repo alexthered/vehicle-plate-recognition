fcf772ce68e6fdc43cfaadbc4f7c77e98399ca04
alexthered <hiep.dv179@gmail.com>
2014-11-26 00:59:52 +0100

    Add plate recognizer class

--- platerecognizer.cpp
+++ platerecognizer.cpp
@@ -1,5 +1,33 @@
 #include "platerecognizer.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-platerecognizer::platerecognizer()
+using namespace cv;
+using namespace std;
+
+//ctor
+PlateRecognizer::PlateRecognizer()
 {
+
+}
+
+
+void PlateRecognizer::RecognizePlate(const cv::Mat& in_plate, std::string& content_plate)
+{
+    cvtColor(in_plate, gray_plate, CV_RGB2GRAY);
+    //extract character's images
+    ExtractCharacterImages(gray_plate);
+
+#if VERBOSE_MODE
+    cv::imshow("Gray image of plate", gray_plate);
+    cv::imshow("Gray image of plate after thresholding", threshold_gray_plate);
+#endif
+}
+
+//separate each character's image from the whole plate image
+void PlateRecognizer::ExtractCharacterImages(const cv::Mat& in_plate_gray)
+{
+    //a simple thresholding
+    cv::threshold(in_plate_gray, threshold_gray_plate, 60, 255, CV_THRESH_BINARY_INV);
+
 }
--- platerecognizer.h
+++ platerecognizer.h
@@ -1,10 +1,27 @@
 #ifndef PLATERECOGNIZER_H
 #define PLATERECOGNIZER_H
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <iostream>
+#include <vector>
 
-class platerecognizer
+/**
+ * @brief Recognize characters from a plate image (letters and numbers)
+ *
+ */
+
+class PlateRecognizer
 {
 public:
-    platerecognizer();
+    PlateRecognizer();
+
+    void RecognizePlate(const cv::Mat& in_plate, std::string& content_plate);
+
+private:
+    void ExtractCharacterImages(const cv::Mat& in_plate_gray);
+
+    cv::Mat gray_plate, threshold_gray_plate;
+    std::vector<cv::Mat> character_img;
 };
 
 #endif // PLATERECOGNIZER_H
