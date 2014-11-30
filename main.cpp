83e09eb35200d07079d60b8020eb1c464df505f8
alexthered <hiep.dv179@gmail.com>
2014-11-30 17:18:29 +0100

    Use QApplication as a wrapper for all processes to take part in

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "app.h"
 #include <QApplication>
 
+
 //main routine
 int main(int argc, char* argv[])
 {
     QApplication MainApp(argc, argv);
 
+    //get the application
     App m_app;
+    m_app.Run();
 
-    return m_app.Run();
+    return MainApp.exec();
 }
