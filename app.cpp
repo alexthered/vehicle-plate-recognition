810bd633eea86e48731076aebbde602bf2284f24
alexthered <hiep.dv179@gmail.com>
2014-12-13 22:29:33 +0100

    Add running mode: CollectMode, TrainMode, RunMode for app::run

--- app.cpp
+++ app.cpp
@@ -1,10 +1,13 @@
 #include "app.h"
+#include <QDirIterator>
 
 App::App():
     QMainWindow()
 {
     p_detector = new PlateDetector();
     p_recognizer = new PlateRecognizer();
+
+    work_mode = CollectMode;
 }
 
 App::~App()
@@ -13,24 +16,95 @@ App::~App()
     delete p_recognizer;
 }
 
-int App::Run()
+int App::LoadImage(const string img_name)
 {
-    /* read input image */
-    Mat in_img = imread("../../data/P6070016.jpg");
-
-    /* Detect plate region within an image (if any) */
-    vector<Mat> plates;
-    p_detector->DetectPlate(in_img, plates);
-
-    std::string content_plate;
-    /* Recognize plate image */
-    for (int i=0; i<plates.size(); i++){
-        std::cout << "Processing plate " << (i+1) << " out of " << plates.size() << std::endl;
-        p_recognizer->RecognizePlate(plates[i], content_plate);
+    in_img = imread(img_name, 1);
+    if (!in_img.data){ //cannot load image
+        cerr << "Cannot load image!" << endl;
+        return 0;
     }
 
-    cv::imshow("Input image", in_img);
-    cv::waitKey(0);
+    return 1;
+}
+
+int App::LoadDirectory(const string path_to_dir)
+{
+    //cout << "[Image Loader] Read images from " << path_to_dir << endl;
+    //convert the directory path to QString
+    QString q_path_to_dir = QString::fromStdString(path_to_dir);
+
+    //iterate over the input directory to load all images
+    QDirIterator dir_iter(QString(q_path_to_dir),
+                          QDir::AllDirs|QDir::Files|QDir::NoSymLinks,
+                          QDirIterator::Subdirectories
+                          );
+
+    while(dir_iter.hasNext())
+    {
+        dir_iter.next();
+
+        //make sure it collects only image files
+        if (dir_iter.fileInfo().completeSuffix().contains("jpg", Qt::CaseInsensitive) ||
+                dir_iter.fileInfo().completeSuffix().contains("jpeg", Qt::CaseInsensitive) ||
+                dir_iter.fileInfo().completeSuffix().contains("png", Qt::CaseInsensitive)
+                ){
+            // get the current file's RELATIVE file path
+            QString current_file = dir_iter.fileInfo().filePath();
+
+            //load image and file and add to the vector
+            Mat cur_img = imread(current_file.toStdString(),1);
+            in_img_vec.push_back(cur_img);
+        }
+    }
+    return 1;
+
+}
+
+int App::Run()
+{
+
+#if (work_mode == CollectMode)
+
+    std::string input_dir_path = "../../data/tmp/";
+    std::string output_dir_path = "../../data/characters";
+
+    //load all image from directory
+    if (!LoadDirectory(input_dir_path))
+        return 0;
+
+    //for each image in the directory, perform plate detection
+    vector<Mat> plates;
+    int counter = 0;
+    vector<Mat>::iterator iter = in_img_vec.begin();
+    while(iter!=in_img_vec.end()){
+        std::cout << "Process " << counter << " out of " << in_img_vec.size() << std::endl;
+        plates.clear();
+        p_detector->DetectPlate((*iter), plates);
+
+        //for each plate image, extract and save character image
+        for (int i=0; i<plates.size();i++){
+            p_recognizer->SaveCharacterImg(plates[i],output_dir_path);
+        }
+        ++counter;
+        ++iter;
+    }
+
+#elif (work_mode == TrainMode )
+
+#elif (work_mode == RunMode )
+    /* read 1 input image */
+    std::string input_name = "../../data/P6070016.jpg";
+    if (!LoadImage(input_name))
+        return 0;
+
+
+    //std::string content_plate;
+    /* Recognize plate image */
+    //for (int i=0; i<plates.size(); i++){
+        //std::cout << "Processing plate " << (i+1) << " out of " << plates.size() << std::endl;
+        //p_recognizer->RecognizePlate(plates[i], content_plate);
+    //}
+#endif
 
     return 1;
 }
--- app.h
+++ app.h
@@ -6,17 +6,41 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <vector>
-
+#include <QMainWindow>
 
 using namespace std;
 using namespace cv;
 
-class App
+enum WorkingMode {
+    UndefinedMode = -1,
+    CollectMode = 0,   //collect character's image to train model
+    TrainMode = 1,     //train model from collected images
+    RunMode = 2        //perform plate detection & recognition on given image
+};
+
+class App : public QMainWindow
 {
 public:
     App();
+    ~App();
 
+    //load a single image
+    int LoadImage(const std::string img_name);
+    //load all images in a directory
+    int LoadDirectory(const std::string path_to_dir);
     int Run();
+
+private:
+    PlateDetector* p_detector;
+    PlateRecognizer* p_recognizer;
+
+    //input data
+    cv::Mat in_img;
+    std::vector<cv::Mat> in_img_vec;
+
+
+    //working mode
+    WorkingMode work_mode;
 };
 
 #endif // APP_H
